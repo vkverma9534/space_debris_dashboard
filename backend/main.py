8bb01ac865367eeb340d8c7c52cbbd39ff928185
unknown <vkverma9534@gmail.com>
2026-08-25 04:12:06 +0530

    Possibly final debugging session

--- backend/main.py
+++ backend/main.py
@@ -34,11 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://space-debris-dashboard.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
